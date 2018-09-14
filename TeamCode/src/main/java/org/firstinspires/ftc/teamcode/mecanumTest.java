92eb3e2d0d21d8d5e86dc0737fe298a48c51667e
Spud <jeffrypig23@gmail.com>
2018-09-14 09:21:19 -0700

    Got a rough program for mecanum drive
    I still need to test the drive directions on the motors btw

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mecanumTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mecanumTest.java
@@ -17,13 +17,55 @@ public class mecanumTest extends LinearOpMode {
         waitForStart();
         while(opModeIsActive()) {
 
-            // right stick is shifting left and right
-            // left is standard drive
+            // Use the old drive code from last year
+            double turn = gamepad1.right_stick_x * gamepad1.right_stick_x, throttle = gamepad1.left_stick_y, slow, powL, powR, turnSpeed=0.7d, sensitivity=0.4d;
+            if (gamepad1.right_bumper) {
+                slow = 0.35f;
+            } else {
+                slow = 1;
+            }
+            if(gamepad1.right_stick_x > 0) {
+                powL = (throttle - turnSpeed*turn);
+                powR = (throttle + turnSpeed*turn);
+            }
 
-            robot.right_back.setPower(gamepad1.left_stick_y * gamepad1.left_stick_x);
-            robot.right_front.setPower(gamepad1.left_stick_y * gamepad1.left_stick_x);
-            robot.left_back.setPower(gamepad1.left_stick_y * gamepad1.left_stick_x);
-            robot.left_front.setPower(gamepad1.left_stick_y * gamepad1.left_stick_x);
+            else {
+                powL = (throttle + turnSpeed*turn);
+                powR = (throttle - turnSpeed*turn);
+            }
+            if (powR > 1.0) {
+                powL -= sensitivity * (powR - 1.0);
+                powR = 1.0;
+            } else if (powL > 1.0) {
+                powR -=  sensitivity * (powL - 1.0);
+                powL = 1.0;
+            } else if (powR < -1.0) {
+                powL +=  sensitivity * (-1.0 - powR);
+                powR = -1.0;
+            } else if (powL < -1.0) {
+                powR +=  sensitivity * (-1.0 - powL);
+                powL = -1.0;
+            }
+
+            // Modify power distribution based on bumpers, for swerve effect
+            if (gamepad1.right_bumper) {
+                robot.right_front.setPower(slow * powR * -1);
+                robot.left_front.setPower(slow * powR * -1);
+                robot.right_back.setPower(slow * powR);
+                robot.left_back.setPower(slow * powL);
+            } else if (gamepad1.left_bumper) {
+                robot.right_back.setPower(slow * powR * -1);
+                robot.left_back.setPower(slow * powR * -1);
+                robot.right_front.setPower(slow * powR);
+                robot.left_front.setPower(slow * powL);
+            } else {
+                robot.right_front.setPower(slow * powR);
+                robot.right_back.setPower(slow * powR);
+                robot.left_front.setPower(slow * powL);
+                robot.left_back.setPower(slow * powL);
+            }
+
+            robot.updateTelemetry(this.telemetry, this.gamepad1);
 
         }
     }
