b484bae823f33187f09fbd8efd2a740536c5eeb3
HenryXII <henryxu1123@gmail.com>
2018-09-20 15:13:56 -0700

    Made drive code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanum_OpMode_Iterative.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanum_OpMode_Iterative.java
@@ -27,9 +27,6 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.firstinspires.ftc.robotcontroller.external.samples;
-
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -50,8 +47,8 @@ import com.qualcomm.robotcore.util.Range;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@TeleOp(name="Basic: Iterative OpMode", group="Iterative Opmode")
-public class BasicOpMode_Iterative extends OpMode
+@TeleOp(name="Mechanum_OpMode_Iterative", group="Iterative Opmode")
+public class mechanum_OpMode_Iterative extends OpMode
 {
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
@@ -60,9 +57,7 @@ public class BasicOpMode_Iterative extends OpMode
     private DcMotor leftDrive2 = null;
     private DcMotor rightDrive2 = null;
 
-    /*
-     * Code to run ONCE when the driver hits INIT
-     */
+
     @Override
     public void init() {
         telemetry.addData("Status", "Initialized");
@@ -86,39 +81,42 @@ public class BasicOpMode_Iterative extends OpMode
         telemetry.addData("Status", "Initialized");
     }
 
-    /*
-     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
-     */
+
     @Override
     public void init_loop() {
     }
 
-    /*
-     * Code to run ONCE when the driver hits PLAY
-     */
+
     @Override
     public void start() {
         runtime.reset();
     }
 
-    /*
-     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
-     */
+
     @Override
     public void loop() {
         // Setup a variable for each drive wheel to save power level for telemetry
-        double leftPower;
-        double rightPower;
+        double left1Power;
+        double right1Power;
+        double left2Power;
+        double right2Power;
+        double allPower =1;
 
         // Choose to drive using either Tank Mode, or POV Mode
         // Comment out the method that's not used.  The default below is POV.
 
         // POV Mode uses left stick to go forward, and right stick to turn.
         // - This uses basic math to combine motions and is easier to drive straight.
-        double drive = -gamepad1.left_stick_y;
-        double turn  =  gamepad1.right_stick_x;
-        leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
-        rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
+
+        double driveForward = gamepad1.left_stick_y;
+        double driveRightSide = gamepad1.left_stick_x;
+
+
+        left1Power    = Range.clip((-driveRightSide + driveForward)*allPower, -1.0, 1.0) ;
+        right1Power   = Range.clip((driveRightSide + driveForward)*allPower, -1.0, 1.0) ;
+        left2Power    = Range.clip((driveRightSide + driveForward)*allPower, -1.0, 1.0) ;
+        right2Power   = Range.clip((-driveRightSide + driveForward)*allPower, -1.0, 1.0) ;
+
 
         // Tank Mode uses one stick to control each wheel.
         // - This requires no math, but it is hard to drive forward slowly and keep straight.
@@ -126,21 +124,20 @@ public class BasicOpMode_Iterative extends OpMode
         // rightPower = -gamepad1.right_stick_y ;
 
         // Send calculated power to wheels
-        leftDrive.setPower(leftPower);
-        rightDrive.setPower(rightPower);
-        leftDrive2.setPower(leftPower);
-        rightDrive2.setPower(rightPower);
+        leftDrive.setPower(left1Power);
+        rightDrive.setPower(right1Power);
+        leftDrive2.setPower(left2Power);
+        rightDrive2.setPower(right2Power);
 
         // Show the elapsed game time and wheel power.
         telemetry.addData("Status", "Run Time: " + runtime.toString());
-        telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+        //telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
     }
 
-    /*
-     * Code to run ONCE after the driver hits STOP
-     */
+
     @Override
     public void stop() {
     }
 
 }
+
