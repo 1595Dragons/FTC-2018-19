2284451471e72203a7ffa1a4f97429353eed4cec
Spud <jeffrypig23@gmail.com>
2018-10-10 16:19:24 -0700

    Added a starter file for vision code example

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Stephens_Vision_Code___yay.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by Stephen Ogden on 10/10/18.
+ * FTC 6128 | 7935
+ * FRC 1595
+ */
+public class Stephens_Vision_Code___yay {
+
+    // TODO: Impliment vision code with lots of comments for Henry and co
+
+
+}
