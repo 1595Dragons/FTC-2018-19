8925d8644b82d5f9a573ae6f392cf5d20f2879d8
Spud <jeffrypig23@gmail.com>
2018-10-29 17:05:57 -0700

    Minor changes to vuforia detection

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuforiaColorDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuforiaColorDetection.java
@@ -1,27 +1,31 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 /**
  * Created by Stephen Ogden on 10/29/18.
  * FTC 6128 | 7935
  * FRC 1595
  */
+@TeleOp(name = "Vuforia color detection", group = "Test")
+@Disabled
 public class VuforiaColorDetection extends LinearOpMode {
 
-    config robot = new config(this.telemetry);
+    private config robot = new config(this.telemetry);
 
     public void runOpMode() {
 
         robot.InitializeVision(this.hardwareMap);
 
         waitForStart();
-
+        robot.StartTrackingVisionTargets();
         while (opModeIsActive()) {
-
 
             robot.updateTelemetry();
 
         }
+        robot.StopTrackingVisionTargets();
     }
 }
