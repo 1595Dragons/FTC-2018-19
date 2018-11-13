f084d0b7985f2e99e07b752148d2ee3697d0e39c
Spud <jeffrypig23@gmail.com>
2018-11-13 12:22:16 -0800

    IT WORK! No idea what the issue was :stuck_out_tongue:

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/detectCubeCrater.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/detectCubeCrater.java
@@ -1,7 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
@@ -60,110 +59,66 @@ public class detectCubeCrater extends LinearOpMode {
                         stage++;
                     }
                     break;
-            }
-
-
-            /*
-            if (stage == 0) {
-                robot.climber.setTargetPosition(robot.maxClimberPos);
-                robot.climber.setPower(1);
-                if (robot.isThere(5, robot.climber)) {
-                    robot.climber.setPower(0);
-                    stage++;
-                }
-            } else if (stage == 1) {
-                robot.driveDistance(MecanumDriveDirection.RIGHT, 3, 1);
-                if (robot.isThere(10, robot.right1, robot.left1, robot.left2, robot.right2)) {
-                    robot.resetMotors(robot.left2, robot.left1, robot.right1, robot.right2);
-                    stage++;
-                }
-            } else if (stage == 2) {
-                robot.climber.setTargetPosition(robot.minClimberPos);
-                robot.climber.setPower(1);
-                if (robot.isThere(5, robot.climber)) {
-                    robot.climber.setPower(0);
-                    stage++;
-                }
-            } else if (stage == 3) {
-                robot.driveDistance(MecanumDriveDirection.FORWARD, 12, 1);
-                if (robot.isThere(10, robot.right1, robot.left1, robot.left2, robot.right2)) {
-                    robot.resetMotors(robot.left2, robot.left1, robot.right1, robot.right2);
-                    stage++;
-                }
-            } else if (stage == 4) {
-                robot.driveDistance(MecanumDriveDirection.LEFT, 3, 1);
-                if (robot.isThere(10, robot.right1, robot.left1, robot.left2, robot.right2)) {
-                    robot.resetMotors(robot.left2, robot.left1, robot.right1, robot.right2);
-                    stage++;
-                }
-            } else if (stage == 5) {
-                time.reset();
-                robot.setupGoldDetector(this.hardwareMap);
-                stage++;
-            } else if (stage == 6) {
-                if (time.seconds() > 3) {
-                    stage++;
-                }
-            } else if (stage == 7) {
-                if (robot.goldDetector.isFound()) {
-                    stage = 16;
-                } else {
-                    stage++;
-                }
-            } else if (stage == 8) {
-                robot.driveDistance(MecanumDriveDirection.LEFT, 16, 1);
-                if (robot.isThere(10, robot.right1, robot.left1, robot.left2, robot.right2)) {
-                    robot.resetMotors(robot.left2, robot.left1, robot.right1, robot.right2);
-                    stage++;
-                }
-            } else if (stage == 9) {
-                time.reset();
-                stage++;
-            } else if (stage == 10) {
-                if (time.seconds() > 3) {
-                    stage++;
-                }
-            } else if (stage == 11) {
-                if (robot.goldDetector.isFound()) {
-                    stage = 16;
-                } else {
-                    stage++;
-                }
-            } else if (stage == 12) {
-                robot.driveDistance(MecanumDriveDirection.RIGHT, 36, 1);
-                if (robot.isThere(10, robot.right1, robot.left1, robot.left2, robot.right2)) {
-                    robot.resetMotors(robot.left2, robot.left1, robot.right1, robot.right2);
-                    stage++;
-                }
-            } else if (stage == 13) {
-                time.reset();
-                stage++;
-                if (stage == 14) {
-                    if (time.seconds() > 3) {
+                case 4:
+                    robot.driveDistance(MecanumDriveDirection.LEFT, 3, 1);
+                    if (robot.isThere(10, robot.right1, robot.left1, robot.left2, robot.right2)) {
+                        robot.resetMotors(robot.left2, robot.left1, robot.right1, robot.right2);
                         stage++;
                     }
-                }
-            } else if (stage == 15) {
-                if (robot.goldDetector.isFound()) {
-                    stage = 16;
-                } else {
+                    break;
+                case 5:
+                    time.reset();
+                    robot.setupGoldDetector(this.hardwareMap);
                     stage++;
-                }
-            } else if (stage == 16) {
-                robot.driveDistance(MecanumDriveDirection.FORWARD, 12, 1);
-                if (robot.isThere(10, robot.right1, robot.left1, robot.left2, robot.right2)) {
-                    robot.resetMotors(robot.left2, robot.left1, robot.right1, robot.right2);
+                    break;
+                case 6:
+                    if (robot.goldDetector.isFound()) {
+                        robot.goldDetector.disable();
+                        stage = 11;
+                    } else if (time.seconds() > 3) {
+                        robot.goldDetector.disable();
+                        stage++;
+                    }
+                    break;
+                case 7:
+                    robot.driveDistance(MecanumDriveDirection.LEFT, 16, 1);
+                    if (robot.isThere(10, robot.right1, robot.left1, robot.left2, robot.right2)) {
+                        robot.resetMotors(robot.left2, robot.left1, robot.right1, robot.right2);
+                        stage++;
+                    }
+                    break;
+                case 8:
+                    time.reset();
+                    robot.goldDetector.enable();
                     stage++;
-                }
-            } else if (stage == 17) {
-                robot.goldDetector.disable();
-                stop();
+                    break;
+                case 9:
+                    if (robot.goldDetector.isFound()) {
+                        robot.goldDetector.disable();
+                        stage = 11;
+                    } else if (time.seconds() > 3) {
+                        robot.goldDetector.disable();
+                        stage++;
+                    }
+                    break;
+                case 10:
+                    robot.driveDistance(MecanumDriveDirection.RIGHT, 32, 1);
+                    if (robot.isThere(10, robot.right1, robot.left1, robot.left2, robot.right2)) {
+                        robot.resetMotors(robot.left2, robot.left1, robot.right1, robot.right2);
+                        stage++;
+                    }
+                    break;
+                case 11:
+                    robot.driveDistance(MecanumDriveDirection.FORWARD, 12, 1);
+                    if (robot.isThere(10, robot.right1, robot.left1, robot.left2, robot.right2)) {
+                        robot.resetMotors(robot.left2, robot.left1, robot.right1, robot.right2);
+                        stage++;
+                    }
+                    break;
             }
-            */
-
-            // TODO: Gets stuck after stage 1. Alsays on "Ready"
 
             telemetry.addData("Stage", stage);
+            telemetry.addData("Time", time.seconds());
             telemetry.addLine();
             robot.updateTelemetry();
 
